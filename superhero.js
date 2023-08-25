ecbb6ef800293c16f2a599daf9f4b2149fb83ac9
linelochmann <limo0001@stud.kea.dk>
2023-08-25 12:44:14 +0200

    g

---
+++ superhero.js
@@ -0,0 +1,17 @@
+{
+  powers: ["LL, Run"];
+  realName: "Sky";
+  alias: "Raccon";
+  origin: "Rome";
+  weaknesses: "Metal";
+  active: "true";
+  isEvil: "true";
+  height: "172";
+  birthday: {
+    day: 19;
+    month: 7;
+    year: 1998;
+  }
+
+  image: "https://youtu.be/2KcNm6ftD7g?si=rvX9GRx7n3afs-F1";
+}
